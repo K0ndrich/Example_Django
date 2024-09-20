1eb363071ec988629a74325e743bb7b6e71b3df2
K0ndrich <123kondrich@gmail.com>
2024-09-20 14:11:20 +0300

    edited

--- my_project/config/settings.py
+++ my_project/config/settings.py
@@ -35,7 +35,8 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # берем значенние переменной среды из .env файла
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-xlc+!ogwdcp14$#_6c6qf7vjbw@ws*^kcrkme7kroovkkcx$nv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True режим дебага включен, нужно выключать при деплое
