3a05fef852aeee3b2bc065da0090a66e1337e7f8
K0ndrich <123kondrich@gmail.com>
2024-09-27 18:39:43 +0300

    edited

--- my_project/my_django/urls.py
+++ my_project/my_django/urls.py
@@ -8,11 +8,17 @@ from my_django.views.main_views import my_view, MyView
 from my_django.views.request_views import my_request_data
 
 urlpatterns = [
-    # name позволяет обращаться к етому url-адресу в коде
-    # вызов представлениея основанное на функции
-    path("my_function_view/", my_view),
+    # вызов представления основанное на функции
+    # name указывает как монжно обращаться  етому пути в html коде
+    # <a href="{% url 'my_function_view' %}" </a>
+    path("my_function_view/", my_view, name="my_function_view"),
+    # 
+    # <a href="{% url 'my_detail_function_view' id %}" </a>
+    path("my_function_view/<int:id>", my_view, name="my_detail_function_view"),
+    # 
     # вызов представлениея основанное на классе
     path("my_classes_view/", MyView.as_view()),
+    # 
     # разбираем наш request из чего состоит
     path("my_request_data/", my_request_data),
     #
